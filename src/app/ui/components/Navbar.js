cccc8d9c396cbc57991a900b559cf5394f7af2e8
Ekenedilichukwu <ekenedilichukwu@Ekenedilichukwus-MacBook-Pro.local>
2022-10-15 17:18:44 +0100

    Building Navbar

--- src/app/ui/components/Navbar.js
+++ src/app/ui/components/Navbar.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="mainDiv">
+      <div>
+        <a href="/">
+          <img src={require("../../images/logo.png")} />
+          <h2>Reclin</h2>
+        </a>
+      </div>
+      <div>
+        <div>
+          <ul>
+            <li>
+              <a href="/aboutus">About us</a>
+            </li>
+            <li>
+              <a>Services</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <a>EN</a>
+            </li>
+            <li>
+              <a href="/login">Login</a>
+            </li>
+            <li>
+              <a href="/signup">Sign Up</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
