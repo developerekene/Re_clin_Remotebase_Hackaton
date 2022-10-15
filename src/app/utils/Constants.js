11e1bdff405e1adcb58e7dbd1dbd064ada80c804
Ekenedilichukwu <ekenedilichukwu@Ekenedilichukwus-MacBook-Pro.local>
2022-10-15 16:42:47 +0100

    Utils folder set up

---
+++ src/app/utils/Constants.js
@@ -0,0 +1,13 @@
+export const COLORS = {
+    baseColorOne: '#1E1E1E',
+    baseColorTwo: '#47C92C',
+    baseColorThree: '#000000',
+    baseColorFour: '#ffffff',
+    baseColorFive: '#1E1E1E',
+    baseColorSix: '#333333'
+}
+
+export const SIZES = {
+    
+}
+
