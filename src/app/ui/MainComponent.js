986ad17e96f5a0d7d69559fc390157927e2ce5a8
Ekenedilichukwu <ekenedilichukwu@Ekenedilichukwus-MacBook-Pro.local>
2022-10-15 16:17:11 +0100

    Component structure

--- src/app/ui/MainComponent.js
+++ src/app/ui/MainComponent.js
@@ -1,8 +1,44 @@
 import React from 'react'
 
+//Importing Components
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Transactions from './pages/Transactions';
+import Navbar from './components/Navbar';
+import Aboutus from './pages/Aboutus';
+import Exchange from './pages/Exchange';
+
 const MainComponent = () => {
+
+    // creating component variable
+    let component;
+
+    // switch statement to call pages and store them in the component variable
+    switch(window.location.pathname){
+        case '/' : component = <Home />;
+        break;
+
+        case '/login' : component = <Login />;
+        break;
+
+        case '/signup' : component = <SignUp />;
+        break;
+
+        case '/exchange' : component = <Exchange />;
+        break;
+
+        case '/transaction' : component = <Transactions />;
+        break;
+
+        case '/aboutus' : component = <Aboutus />;
+        break;
+    }
   return (
-    <div>MainComponent</div>
+    <>
+    <Navbar />
+    {component}
+    </>
   )
 }
 
---
+++ src/app/ui/components/RadioButtons.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RadioButtons = () => {
+  return (
+    <div>RadioButtons</div>
+  )
+}
+
+export default RadioButtons
