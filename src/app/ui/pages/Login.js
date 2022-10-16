ce7e6b10bf5f3250f8c76dd7c018d9d745978b4e
Ekenedilichukwu <ekenedilichukwu@Ekenedilichukwus-MacBook-Pro.local>
2022-10-16 06:00:01 +0100

    Login component completed

--- src/app/ui/pages/Login.js
+++ src/app/ui/pages/Login.js
@@ -5,29 +5,63 @@ const Login = () => {
     <div>
       <div className="loginDiv">
         <div>
-          <h2>Login</h2>
+          <h2 className="textHeader">Login</h2>
         </div>
-        <div>
-          <input placeholder="Email"></input>
-          <input placeholder="Password 8+ characters"></input>
+        <div className="inputDiv">
+          <input
+            style={{
+              width: 460,
+              height: 58,
+              borderRadius: 8,
+              paddingLeft: 16,
+              color: "rgba(51, 51, 51, 0.8)",
+            }}
+            placeholder="Email"
+          />
         </div>
-        <div>
+        <div className="inputDiv">
+          <input
+            style={{
+              width: 460,
+              height: 58,
+              borderRadius: 8,
+              paddingLeft: 16,
+              color: "rgba(51, 51, 51, 0.8)",
+            }}
+            placeholder="Password 8+ characters"
+          />
+        </div>
+
+        <div className="signupDiv">
           <p>Forgot Password</p>
-          <button>Login</button>
+          <button
+            style={{
+              marginTop: 24,
+              width: "100%",
+              height: 56,
+              color: "#ffffff",
+              backgroundColor: "#47c92c",
+              borderWidth: 0,
+              borderRadius: 10
+            }}
+          >
+            Login
+          </button>
         </div>
-        <div className="signup">
+
+        <div className="signupDivTwo">
           <p>Don't have an account?</p>
           <p>SignUp</p>
         </div>
         <div className="or">
-          <img src={require('../../images/Line.png')}/>
+          <img src={require("../../images/Line.png")} />
           <p>OR</p>
-          <img src={require('../../images/Line.png')}/>
+          <img src={require("../../images/Line.png")} />
         </div>
 
         <div className="google">
-        <img src={require('../../images/google.png')} onClick={{}}/>
-        <p>Contuine with Google</p>
+          <img src={require("../../images/google.png")} onClick={{}} />
+          <p>Contuine with Google</p>
         </div>
       </div>
     </div>
